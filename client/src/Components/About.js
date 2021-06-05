3ce8e4773f705f7d628ff09e284da1a9b6a29851
Caleb Martin <caleb637@icloud.com>
2021-06-04 21:37:10 -0400

    finished design for all pages

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,7 +1,69 @@
 import React from 'react'
 
+import Nav from './Nav'
+
+import '../css/About.css'
+
 function About() {
-  return <div></div>
+  return (
+    <div>
+      <Nav />
+      <h1 className='about__heading'>About</h1>
+      <div className='about'>
+        <div className='about__container'>
+          <h2 className='about__h2'>About the Owner</h2>
+          <p className='about__text-content'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum! Provident
+            similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+            eius earum ut molestias architecto voluptate aliquam nihil, eveniet
+            aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
+            error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias
+            eos sapiente officiis modi at sunt excepturi expedita sint? Sed
+            quibusdam recusandae alias error harum maxime adipisci amet laborum.
+            Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates
+            a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius
+            fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero
+            magni deleniti quod quam consequuntur! Commodi minima excepturi
+            repudiandae velit hic maxime doloremque. Quaerat provident commodi
+            consectetur veniam similique ad earum omnis ipsum saepe, voluptas,
+            hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+            cupiditate excepturi mollitia maiores labore
+          </p>
+        </div>
+        <div className='about__container'>
+          <h2 className='about__h2'>The Story</h2>
+          <p className='about__text-content'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum! Provident
+            similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+            eius earum ut molestias architecto voluptate aliquam nihil, eveniet
+            aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
+            error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias
+            eos sapiente officiis modi at sunt excepturi expedita sint? Sed
+            quibusdam recusandae alias error harum maxime adipisci amet laborum.
+            Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates
+            a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius
+            fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero
+            magni deleniti quod quam consequuntur! Commodi minima excepturi
+            repudiandae velit hic maxime doloremque. Quaerat provident commodi
+            consectetur veniam similique ad earum omnis ipsum saepe, voluptas,
+            hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+            cupiditate excepturi mollitia maiores labore
+          </p>
+        </div>
+      </div>
+      <footer className='footer_copyright'>
+        Hosted by De Stefano Enterprises, LLC, all rights reserved
+      </footer>
+    </div>
+  )
 }
 
 export default About
--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
+import Nav from './Nav'
+
+import '../css/Contact.css'
+
 function Contact() {
-  return <div></div>
+  return (
+    <div className='contact'>
+      <Nav />
+      <h1 className='contact__heading'>Contact</h1>
+      <span className='contact__sub'>
+        Email me with any inquiries or call at 717-826-3754. I would be happy to
+        answer any questions you may have!
+      </span>
+      <form>
+        <input type='text' placeholder='Name' id='input-name' />
+        <input type='text' placeholder='Email' id='input-email' />
+        <input type='text' placeholder='Subject' id='input-subject' />
+        <textarea placeholder='Message' id='input-message'></textarea>
+        <input type='submit' value='Send Message' />
+      </form>
+
+      <footer className='footer_copyright'>
+        Hosted by De Stefano Enterprises, LLC, all rights reserved
+      </footer>
+    </div>
+  )
 }
 
 export default Contact
