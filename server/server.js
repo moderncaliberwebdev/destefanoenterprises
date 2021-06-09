f06e9f0db1efa0bb7edb453421ef7cbfcda61f40
Caleb Martin <caleb637@icloud.com>
2021-06-09 14:24:19 -0400

    prepared for deployment to heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Imports
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 
 // Routes
 import routes from './routes/router.js'
@@ -12,5 +13,15 @@ app.use(express.json())
 dotenv.config()
 
 app.use('/api', routes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/dist')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  )
+}
 
 app.listen(PORT, () => console.log('Server is running on port ', PORT))
